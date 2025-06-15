656f73255af2224a4774bc43e6a03cce164a03a3
YJSNPI\3ma0408 <yagi0629yu@gmail.com>
2025-06-15 21:47:17 +0900

    @Autowired追記

--- src/main/java/com/example/demo/controller/ReserveController.java
+++ src/main/java/com/example/demo/controller/ReserveController.java
@@ -1,22 +1,31 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.entity.Room;
+import com.example.demo.repository.RoomRepository;
+
 @Controller
+
 public class ReserveController {
+
+	@Autowired
+	private RoomRepository roomRepository;
 
 	//予約フォームの表示 
 	@GetMapping("/rooms/{id}/reserve")
 	public String reserveIndex(
-			@PathVariable("id") Long id,
+			@PathVariable("id") Integer id,
 			@RequestParam(required = false) String error,
 			Model model) {
+		Room room = roomRepository.findById(id).get();
 
-		model.addAttribute("roomId", id);
+		model.addAttribute("room", room);
 		model.addAttribute("error", error);
 		return "reserve";
 	}
