b9ab430ba1d15c534b1a9a8e2e2d01def13d13d4
sakumatakumi <goose.326.house.bibi@gmail.com>
2025-06-15 14:42:20 +0900

    entityの名前変更に伴いリポジトリー等も変更。

--- src/main/java/com/example/demo/controller/RoomController.java
+++ src/main/java/com/example/demo/controller/RoomController.java
@@ -1,13 +1,42 @@
 package com.example.demo.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.Room;
+import com.example.demo.repository.RoomRepository;
 
 @Controller
 public class RoomController {
 
+	@Autowired
+	RoomRepository roomRepository;
+
 	@GetMapping("/room")
-	public String showRoomList() {
+	public String showRoomList(
+			@RequestParam(required = false) String keyword,
+			@RequestParam(required = false, defaultValue = "0") Integer underPrice,
+			@RequestParam(required = false, defaultValue = "100000") Integer upPrice,
+			@RequestParam(required = false) LocalDate checkinDate,
+			@RequestParam(required = false) LocalDate checkoutDate,
+			Model model) {
+
+		List<Room> rooms = null;
+
+		rooms = roomRepository.findAll();
+
+		model.addAttribute("rooms", rooms);
+		model.addAttribute("keyword", keyword);
+		model.addAttribute("price", underPrice);
+		model.addAttribute("price", upPrice);
+		model.addAttribute("checkinDate", checkinDate);
+		model.addAttribute("checkoutDate", checkoutDate);
 		return "top";
 	}
 }
