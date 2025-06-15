d9feb9a8df4f03db82a2887bf0a7b695ae300cdc
YJSNPI\3ma0408 <yagi0629yu@gmail.com>
2025-06-15 21:20:32 +0900

    予約フォームの表示機能を追加

--- src/main/java/com/example/demo/controller/ReserveController.java
+++ src/main/java/com/example/demo/controller/ReserveController.java
@@ -1,8 +1,24 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ReserveController {
+
+	//予約フォームの表示 
+	@GetMapping("/rooms/{id}/reserve")
+	public String reserveIndex(
+			@PathVariable("id") Long id,
+			@RequestParam(required = false) String error,
+			Model model) {
+
+		model.addAttribute("roomId", id);
+		model.addAttribute("error", error);
+		return "reserve";
+	}
 
 }
