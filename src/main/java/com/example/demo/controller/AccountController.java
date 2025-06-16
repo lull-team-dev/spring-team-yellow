3ffeeeee457401bd482f8572325eeef03684e2ee
lull-y-miyazaki <y.miyazaki@lull-inc.co.jp>
2025-06-16 20:59:38 +0900

    feat:会員登録処理の実装

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -2,9 +2,15 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.demo.entity.Guest;
 import com.example.demo.repository.GuestRepository;
 
 @Controller
@@ -18,6 +24,25 @@ public class AccountController {
 	@GetMapping("/register")
 	public String accountIndex() {
 		return "register";
+	}
+
+	// 新規会員登録処理
+	@PostMapping("/register")
+	public String addAccount(@ModelAttribute Guest guest,
+			@RequestParam String email,
+			@RequestParam String password_confirm,
+			RedirectAttributes redirectAttributes,
+			Model model) {
+
+		if (!guest.getPassword().equals(password_confirm)) {
+			model.addAttribute("error", "パスワードが一致しません");
+			return "register";
+		}
+
+		guestRepository.save(guest);
+		redirectAttributes.addFlashAttribute("email", email);
+
+		return "redirect:/login";
 	}
 
 }
