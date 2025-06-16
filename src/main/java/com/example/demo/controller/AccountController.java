ce7e4911e7ee569ae22c00b9a2b2381f6299b6e7
lull-y-miyazaki <y.miyazaki@lull-inc.co.jp>
2025-06-16 20:54:09 +0900

    画面表示の処理追加

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -1,8 +1,23 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.repository.GuestRepository;
 
 @Controller
+@RequestMapping("/account")
 public class AccountController {
+
+	@Autowired
+	GuestRepository guestRepository;
+
+	// 新規会員登録画面の表示
+	@GetMapping("/register")
+	public String accountIndex() {
+		return "register";
+	}
 
 }
