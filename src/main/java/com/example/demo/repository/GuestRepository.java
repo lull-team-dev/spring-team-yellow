8c002241c173c62c90b3f692039f3e94f35a7ca3
lull-y-miyazaki <y.miyazaki@lull-inc.co.jp>
2025-07-02 14:53:20 +0900

    feat: 登録時にメールアドレスの重複チェックを追加

--- src/main/java/com/example/demo/repository/GuestRepository.java
+++ src/main/java/com/example/demo/repository/GuestRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +9,8 @@ import com.example.demo.entity.Guest;
 
 public interface GuestRepository extends JpaRepository<Guest, Integer> {
 	List<Guest> findByEmailAndPassword(String email, String password);
+
+	// メールアドレスの重複チェック用クエリメソッド
+	Optional<Guest> findByEmail(String email);
 
 }
