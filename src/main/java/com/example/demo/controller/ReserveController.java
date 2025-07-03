d80158e9aadc5adfb31d6f21c0f3c46768bf2326
永木媛夏 <himeka.8170@gmail.com>
2025-07-03 17:00:18 +0900

    人数表記追加

--- src/main/java/com/example/demo/controller/ReserveController.java
+++ src/main/java/com/example/demo/controller/ReserveController.java
@@ -96,7 +96,7 @@ public class ReserveController {
 
 		//リダイレクトの場合内容を送られてきた情報をそのままセット
 		if (guestName != null && !guestName.isBlank()) {
-			dto.setGuestName(guestName);
+			dto.setGuestName(guestName);	
 		} else {
 			dto.setGuestName(guest.getName());
 		}
