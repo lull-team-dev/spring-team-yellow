a9c25b8d5a6ae19d2f5f796242df99fa20c21a0f
永木媛夏 <himeka.8170@gmail.com>
2025-07-17 14:04:20 +0900

    不要なコード削除

--- src/main/java/com/example/demo/controller/RoomLikeController.java
+++ src/main/java/com/example/demo/controller/RoomLikeController.java
@@ -60,34 +60,4 @@ public class RoomLikeController {
 		return "like";
 	}
 
-	//	お気に入り処理
-	//	@GetMapping("/like/{id}")
-	//	public String like(@PathVariable("id") Integer id,
-	//			HttpServletRequest request,
-	//			Model model) {
-	//
-	//		likeService.toggleLike(account.getId(), id);
-	//
-	//		String referer = request.getHeader("Referer");
-	//		return "redirect:" + referer + '#' + id;
-	//	}
-	//
-	//	@GetMapping("/rooms/{id}/like")
-	//	public String roomsLike(@PathVariable("id") Integer id,
-	//			HttpServletRequest request,
-	//			Model model) {
-	//
-	//		likeService.toggleLike(account.getId(), id);
-	//
-	//		String referer = request.getHeader("Referer");
-	//		return "redirect:" + referer + '#' + id;
-	//	}
-
-	//	//	お気に入り処理
-	//	@GetMapping("/like/{id}/like")
-	//	public String like(@PathVariable("id") Integer id,
-	//			Model model) {
-	//		likeService.toggleLike(account.getId(), id);
-	//		return "redirect:/like#" + id;
-	//	}
 }
